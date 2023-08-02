3acbf3223e126c3da668529c5b94f2a35c1e1dcb
zoltancsik <csik@2txt.de>
2023-08-02 17:57:13 +0200

    Move Functions to Entities

--- MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/main/entities.kt
+++ MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/main/entities.kt
@@ -2,6 +2,7 @@ package furhatos.app.movieinterview.flow.main
 
 import furhatos.nlu.EnumEntity
 import furhatos.util.Language
+import kotlin.random.Random
 
 class Genres : EnumEntity(speechRecPhrases = true) {
     override fun getEnum(lang: Language): List<String> {
@@ -10,4 +11,43 @@ class Genres : EnumEntity(speechRecPhrases = true) {
             "thriller", "western")
     }
 }
+
+fun getMovieByGenre(genre: String): String {
+    val moviesByGenre = mapOf(
+        "action" to "Die hard, John Wick, The Matrix",
+        "adventure" to "Indiana Jones, Avatar, Pirates of the Caribbean",
+        "animation" to "Toy Story, Shrek, Kung-Fu Panda",
+        "comedy" to "Monty Python and the Holy Grail, Hangover, Borat",
+        "crime/mystery" to "James Bond, The Sixth Sense, The Green Mile",
+        "drama" to "12 years a slave, Billy Elliot, Gladiator",
+        "fantasy" to "Lord of the Rings, Thor, Willy Wonka and the chocolate fabric",
+        "horror" to "Saw, The Shining, The Ring",
+        "romance" to "La la land, Titanic, When Harry met Sally",
+        "sci-fi" to "Dune, Star Wars, The Terminator",
+        "thriller" to "The silence of the lambs, The prestige, Everything everywhere all at once",
+        "western" to "The good, the bad and the ugly, No country for old man, Django unchained"
+    )
+    val moviesString = moviesByGenre[genre] ?: return "I don't really know any movies in that genre."
+    val moviesList = moviesString.split(", ")
+    return moviesList[Random.nextInt(moviesList.size)]
+}
+
+fun getplotbyMovie(movie: String): String {
+    val moviesByGenre = mapOf(
+        "John Wick" to "A guy avenging the death of his beloved dog.",
+        "The Matrix" to "Some sci-fi stuff that I never really understood.",
+        "Die hard" to "A random guy just always killing everyone."
+    )
+    return moviesByGenre[movie] ?: "I don't really know the plot of that movie either. It was a long time ago"
+}
+
+fun getactorbyMovie(movie: String): String {
+    val moviesByGenre = mapOf(
+        "John Wick" to "Keanu Reeves",
+        "The Matrix" to "Keanu Reeves",
+        "Die hard" to "Bruce Willis."
+    )
+    return moviesByGenre[movie] ?: "I don't really remember either"
+}
+
 
