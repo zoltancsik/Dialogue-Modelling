679e647defdfeb85a1d43722e8274149948dfbd9
zoltancsik <csik@2txt.de>
2023-08-02 12:27:33 +0200

    Add AskMovie States

--- MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/dialogueflow.kt
+++ MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/dialogueflow.kt
@@ -7,9 +7,33 @@ val AskMovieState: State = state(Parent) {
         furhat.ask("What was the last movie you watched?")
     }
     onResponse {
-        furhat.ask("What do you remember about it?")
+        goto(RememberMovieState)
     }
     onNoResponse {
         furhat.ask("The last movie you have watched, what was it?")
+    }
+}
+
+val RememberMovieState: State = state(Parent) {
+    onEntry {
+        furhat.ask("What do you remember about it?")
+    }
+    onResponse{
+        goto(RecommendMovieState)
+    }
+    onNoResponse {
+        furhat.ask("What was memorable about the movie?")
+    }
+}
+
+val RecommendMovieState: State = state(Parent) {
+    onEntry {
+        furhat.ask("Would you recommend this movie?")
+    }
+    onResponse{
+        furhat.ask("Why is that?")
+    }
+    onNoResponse {
+        furhat.ask("Would you recommend this movie?")
     }
 }
