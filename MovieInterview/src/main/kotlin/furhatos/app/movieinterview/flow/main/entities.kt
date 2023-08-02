e77a49be3e128ec477c755a8d47b12ab93ab661a
zoltancsik <csik@2txt.de>
2023-08-02 13:35:43 +0200

    Add ListEntity Genres

---
+++ MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/main/entities.kt
@@ -0,0 +1,15 @@
+package furhatos.app.movieinterview.flow.main
+
+import furhatos.nlu.EnumEntity
+import furhatos.nlu.ListEntity
+import furhatos.util.Language
+
+class ListofGenres : ListEntity<Genres>()
+
+class Genres : EnumEntity(speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("action", "adventure", "animation", "comedy", "crime",
+                      "drama", "fantasy", "horror", "romance", "sci-fi",
+                      "thriller", "western")
+    }
+}
