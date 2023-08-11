d6ed266d09594bd9d23cbb060c585fa2b2883ef2
zoltancsik <csik@2txt.de>
2023-08-11 16:09:01 +0200

    Add delay

--- MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/parent.kt
+++ MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/parent.kt
@@ -2,8 +2,16 @@ package furhatos.app.movieinterview.flow
 
 import furhatos.app.movieinterview.flow.main.Idle
 import furhatos.flow.kotlin.*
+import furhatos.gestures.Gestures
 
 val Parent: State = state {
+    onInterimResponse(endSil = 500) {
+        // Give some feedback, indicating that FurHat is listening.
+        random (
+            { furhat.gesture(Gestures.Nod) },
+            { furhat.gesture(Gestures.Thoughtful) }
+        )
+    }
 
     onUserEnter(instant = true) {
          furhat.glance(it)
