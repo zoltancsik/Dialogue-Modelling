014c9f15d6b7544444487940a7cd8ba219d0ca3b
zoltancsik <csik@2txt.de>
2023-08-11 16:17:52 +0200

    Refine Entities

--- MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/main/entities.kt
+++ MovieInterview/src/main/kotlin/furhatos/app/movieinterview/flow/main/entities.kt
@@ -6,9 +6,9 @@ import java.time.LocalTime
 
 class Genres : EnumEntity(speechRecPhrases = true) {
     override fun getEnum(lang: Language): List<String> {
-        return listOf("action", "adventure", "animation", "comedy", "crime",
-            "drama", "fantasy", "horror", "romance", "sci-fi",
-            "thriller", "western")
+        return listOf("action", "adventure", "animation", "comedy",
+            "crime", "documentary", "drama", "fantasy", "horror",
+            "romance", "sci-fi", "thriller", "western")
     }
 }
 
@@ -19,6 +19,7 @@ fun getWorstMovieByGenre(genre: String): String {
         "animation" to "Sunflower",
         "comedy" to "Monty Python and the Holy Grail",
         "crime/mystery" to "James Bond",
+        "documentary" to "Finding Home: Journey to MLB",
         "drama" to "12 years a slave",
         "fantasy" to "Lord of the Rings",
         "horror" to "Saw",
@@ -34,32 +35,23 @@ fun getWorstMovieByGenre(genre: String): String {
 
 fun getBestMovieByGenre(genre: String): String {
     val moviesByGenre = mapOf(
-        "action" to "The Flash",
-        "adventure" to "Polaris, the Space Submarine and the Mystery of the Polar Night",
-        "animation" to "Miraculous: Ladybug & Cat Noir, The Movie",
-        "comedy" to "Monty Python and the Holy Grail",
-        "crime/mystery" to "James Bond",
-        "drama" to "12 years a slave",
-        "fantasy" to "Lord of the Rings",
-        "horror" to "Saw",
-        "romance" to "La la land",
+        "action" to "The Lord of the Rings: The Return of the King",
+        "adventure" to "Everything Everywhere All at Once",
+        "animation" to "WALL-E",
+        "comedy" to "Bird man",
+        "crime/mystery" to "No Country for Old Men",
+        "documentary" to "Citizen four",
+        "drama" to "Moonlight",
+        "fantasy" to "The Shape of Water",
+        "horror" to "Parasite",
+        "romance" to "The Artist",
         "sci-fi" to "Dune",
         "science fiction" to "Dune",
-        "thriller" to "The silence of the lambs",
-        "western" to "The good, the bad and the ugly"
+        "thriller" to "The Departed",
+        "western" to "Dances with Wolves"
     )
 
     return moviesByGenre[genre] ?: "I don't really know any movies in that genre."
-}
-
-
-fun getactorbyMovie(movie: String): String {
-    val moviesByGenre = mapOf(
-        "John Wick" to "Keanu Reeves",
-        "The Matrix" to "Keanu Reeves",
-        "Die hard" to "Bruce Willis."
-    )
-    return moviesByGenre[movie] ?: "I don't really remember either"
 }
 
 fun getTimeBasedGreeting(): String {
