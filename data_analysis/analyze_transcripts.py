a0c116731dd60b928b2006b0e49a33e7d7a6f58a
zoltancsik <csik@2txt.de>
2023-09-06 14:30:59 +0200

    Refine Calculations

--- data_analysis/analyze_transcripts.py
+++ data_analysis/analyze_transcripts.py
@@ -45,9 +45,11 @@ if __name__ == "__main__":
     for line in filtered_lines:
         unique_words_per_line = dialogue.count_unique_words(line)
         total_unique_words += unique_words_per_line
-        print(line, end='')
-        print("NUM:", unique_words_per_line)
+        # print(line, end='')
+        # print("NUM:", unique_words_per_line)
         
     average_unique_words = total_unique_words / len(filtered_lines) if filtered_lines else 0
-    print(f"Average number of unique words per sentence: {round(average_unique_words,2)}, for {len(filtered_lines)} sentences")
-    print(f"Calculation: {total_unique_words}/{len(filtered_lines)}")
+    print(f"Total number of sentences: {len(filtered_lines)}.")
+    print(f"Average number of unique words per sentence: {round(average_unique_words,2)}.")
+    print(f"Calculation: sum_of_unique_value/total_number_of_sentences.")
+    print(f"In Numbers: {total_unique_words}/{len(filtered_lines)}.")
