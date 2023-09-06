d28c19982215ab69ac20d4e7bf43ebe8ea3f9eb6
zoltancsik <csik@2txt.de>
2023-09-06 13:32:51 +0200

    Add DocStrings

--- data_analysis/analyze_transcripts.py
+++ data_analysis/analyze_transcripts.py
@@ -6,10 +6,26 @@ class DataAnalysis:
         self.lines = lines
 
     def filter_B(self):
+        """
+        Filters and returns lines from the instance's `lines` attribute that start with 'B: '. 
+        The 'B: ' prefix is removed from the returned lines.
+
+        Returns:
+        - list of str: A list of lines that originally started with 'B: '.
+        """
         filtered_lines = [line[3:] for line in self.lines if line.startswith('B: ')]
         return filtered_lines
     
-    def count_unique_words(self, lines): 
+    def count_unique_words(self, lines):
+        """
+        Count the number of unique words in the provided lines, excluding certain punctuations.
+        
+        Parameters:
+        - lines (str): The sentence to be analyzed.
+
+        Returns:
+        - int: The number of unique words found in the provided lines, excluding '.', ',', or '?'.
+        """
         words = word_tokenize(lines)
         words_stripped = []
         for char in words:
