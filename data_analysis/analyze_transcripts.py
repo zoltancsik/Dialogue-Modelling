343351a183a86065e25fd9cf346118d68156eba9
zoltancsik <csik@2txt.de>
2023-09-06 10:31:14 +0200

    Use string slicing on output

--- data_analysis/analyze_transcripts.py
+++ data_analysis/analyze_transcripts.py
@@ -3,9 +3,16 @@ class DataAnalysis:
         self.filename = filename
 
     def filter_B(self):
+        """
+        Filters the lines from the file specified in self.filename to only include lines of the dialogue
+        that start with 'B: '. The returned lines exclude the 'B: ' prefix, creating data that can be analyzed.
+
+        Returns:
+            list: A list of filtered lines from the dialogue without the 'B: ' prefix.
+        """
         with open(self.filename, 'r') as file:
             lines = file.readlines()
-            filtered_lines = [line for line in lines if line.startswith('B: ')]
+            filtered_lines = [line[3:] for line in lines if line.startswith('B: ')]
         return filtered_lines
 
 if __name__ == "__main__":
@@ -14,5 +21,3 @@ if __name__ == "__main__":
     filtered_lines = dialogue.filter_B()
     for line in filtered_lines:
         print(line, end='')
-
-# Start Working on Lexical Diversity
