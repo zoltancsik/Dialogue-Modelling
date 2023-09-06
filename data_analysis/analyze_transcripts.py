832311d47a990f3b78c81c2663369ca0469636de
zoltancsik <csik@2txt.de>
2023-09-06 10:21:47 +0200

    Add first python script

--- data_analysis/analyze_transcripts.py
+++ data_analysis/analyze_transcripts.py
@@ -1,1 +1,17 @@
-# This will be used to analyse transcribed data
+class DataAnalysis:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def filter_B(self):
+        with open(self.filename, 'r') as file:
+            lines = file.readlines()
+            filtered_lines = [line for line in lines if line.startswith('B: ')]
+        return filtered_lines
+
+if __name__ == "__main__":
+    dialogue = DataAnalysis('../transcribed_data/HC1.txt')
+    
+    # Get all lines with 'B: ' and print them
+    filtered_lines = dialogue.filter_B()
+    for line in filtered_lines:
+        print(line, end='')
