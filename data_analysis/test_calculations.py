8dd52d1cb554d5634e38154d257cd871d5143198
zoltancsik <csik@2txt.de>
2023-09-07 19:16:25 +0200

    Add Average Unique Words

--- data_analysis/test_calculations.py
+++ data_analysis/test_calculations.py
@@ -10,3 +10,9 @@ def test_count_words():
     da = DataAnalysis([])
     assert da.count_words("This is a sentence with seven words.") == 7
     assert da.count_words("Five words in this one?") == 5
+
+def test_count_unique_words():
+    # Test, whether the correct number of unique words is counted
+    da = DataAnalysis([])
+    assert da.count_unique_words("Well well you") == 2
+    assert da.count_unique_words("One, two, two, three!") == 3
