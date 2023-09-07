e97c8e98d9ef362544b86cb34d9f2323c6b0f3c7
zoltancsik <csik@2txt.de>
2023-09-07 19:16:06 +0200

    Add Average Word Test

--- data_analysis/test_calculations.py
+++ data_analysis/test_calculations.py
@@ -4,3 +4,9 @@ def test_filter_B():
     # Test, whether filtering out the participants's sentences only work.
     da = DataAnalysis(["B: Hello", "A: World", "B: How are you?"])
     assert da.filter_B() == ["Hello", "How are you?"]
+
+def test_count_words():
+    # Test, whether the correct number of words is counted in a sentence:
+    da = DataAnalysis([])
+    assert da.count_words("This is a sentence with seven words.") == 7
+    assert da.count_words("Five words in this one?") == 5
