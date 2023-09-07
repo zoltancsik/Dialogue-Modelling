fdece0c9b9b14ad3c00260947f0bb55daba554a7
zoltancsik <csik@2txt.de>
2023-09-07 19:39:14 +0200

    add test_tokenize_line test

--- data_analysis/test_calculations.py
+++ data_analysis/test_calculations.py
@@ -1,5 +1,6 @@
 from analyze_transcripts import DataAnalysis
 from utilities import strip_sentence
+from utilities import remove_contractions
 
 def test_filter_B():
     # Test, whether filtering out the participants's sentences only work.
@@ -19,6 +20,14 @@ def test_count_unique_words():
     assert da.count_unique_words("If, if I say, If I agree, I help?") == 5
 
 def test_sentence_length():
+    # Test, whether the correct length of a sentence, based on characters is counted.
+    # Account for strip_sentence removing characterst that are not supposed to be counted.
     da = DataAnalysis([])
     assert da.sentence_length(strip_sentence("I don't know if it was the very first time.")) == 32
     assert da.sentence_length(strip_sentence("Which movie, can you repeat?")) == 22
+
+def test_tokenize_line():
+    # Test, whether the input is tokenized correctly and whether punctuations and contractions are handled correctly.
+    da = DataAnalysis([])
+    assert da.tokenize_line("Would you be interested in watching it again?") == ["Would", "you", "be", "interested", "in", "watching", "it", "again"]
+    assert da.tokenize_line("I did not hear anything..") == ["I", "did", "not", "hear", "anything", ".."]
