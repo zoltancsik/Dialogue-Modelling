ff28197db5dbbabb3e2fda81b2a9f8a19cb68622
zoltancsik <csik@2txt.de>
2023-09-07 19:10:57 +0200

    Add First Test

--- data_analysis/test_calculations.py
+++ data_analysis/test_calculations.py
@@ -1,19 +1,6 @@
-from analyze_transcripts import calculate_sentence_length
 from analyze_transcripts import DataAnalysis
 
-def test_calculate_sentence_length():
-    
-    # Example Data
-    string = ["one."]
-    dialogue = DataAnalysis(string)
-    filtered_lines = dialogue.filter_B()
-    
-    # Expected Result:
-    expected_rttr = 3
-    
-    # Running the function
-    result = calculate_sentence_length(filtered_lines)
-    print(result)
-    
-    # Assertion
-    assert result == expected_rttr
+def test_filter_B():
+    # Test, whether filtering out the participants's sentences only work.
+    da = DataAnalysis(["B: Hello", "A: World", "B: How are you?"])
+    assert da.filter_B() == ["Hello", "How are you?"]
